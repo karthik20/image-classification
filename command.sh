8fe2ead61fb70e01b29a965bc01e927cda004c85
karthik20 <aceventura18@gmail.com>
2017-08-09 14:12:29 +0800

    Initial commit of image-classification in floydhub

---
+++ command.sh
@@ -0,0 +1,4 @@
+pip install -r floyd_requirements.txt
+cp -r /code/* /output
+cd /output
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/yDh7ujSg6dXgUyPGMy67S3' --NotebookApp.token='' --NotebookApp.allow_origin='*'
